0dcd7f59cd3fbabde47f1bd8ff43a8fcc6317836
Harley Torrisi <mail@harleycodes.com>
2024-06-29 08:32:10 +1000

    Updated to NexttJS 14

--- next.config.js
+++ next.config.js
@@ -8,11 +8,6 @@ const nextConfig = {
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'src', 'styles')],
 	},
-	experimental: {
-		// TODO: Followup and make sure all security concerns are addressed.
-		// https://nextjs.org/docs/app/building-your-application/data-fetching/forms-and-mutations#convention
-		serverActions: true,
-	},
 }
 
 module.exports = nextConfig
